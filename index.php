bb1f987bf8ac2c219a170c320df63566f68d770e
Muze-mac <13581261474@163.com>
2023-03-27 20:20:28 +0800

    测试

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ function crb_attach_theme_options()
     Container::make('theme_options', __('Theme Options'))
         ->add_fields(array(
             Field::make('text', 'crb_phone_numbers', 'Text Field'),
-            Field::make('text', 'crb_phone_numberss', 'Text Field'),
+            Field::make('text', 'crb_phone_numberss', 'Text Field')
+                ->set_visible_in_rest_api($visible = true),
         ));
 }
 //定义 REST API 端点（Endpoint）
@@ -23,9 +24,9 @@ function mytheme_register_rest_endpoints()
     register_rest_route('mytheme/v1', 'theme-options', array(
         'methods' => 'GET',
         'callback' => 'mytheme_get_theme_options',
-        'permission_callback' => function () {
-            return current_user_can('manage_options');
-        },
+        //'permission_callback' => function () {
+        //    return current_user_can('manage_options');
+        //},
     ));
 
     // Update theme options
@@ -60,10 +61,30 @@ function mytheme_update_theme_options($request)
 {
     if (isset($request['my_option'])) {
         $options = $request->get_params();
+
         $options = array_merge(carbon_get_theme_option(), $options);
+
         Carbon_Admin::update_site_options($options);
         return '主题选项更新成功！';
     } else {
         return '没有对主题选项进行更新。'+$request['my_option'];
     }
+}
+
+function mytheme_update_theme_optionsssss(WP_REST_Request $request)
+{
+    $params = $request->get_params();
+
+    foreach ($params as $key => $value) {
+        // Sanitize the option values as needed
+        $sanitized_value = sanitize_text_field($value);
+
+        // Update the option value in Carbon Fields
+        Carbon_Admin::update_site_option($key, $sanitized_value);
+    }
+
+    return array(
+        'success' => true,
+        'message' => 'Options updated successfully',
+    );
 }
