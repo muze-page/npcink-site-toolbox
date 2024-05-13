feff95b8bb09cfe134547581bc6ed674b5df1c65
Muze-mac <13581261474@163.com>
2024-05-13 13:44:16 +0800

    调整彩色标签云功能位置

--- admin/partials/page/comment/index.php
+++ admin/partials/page/comment/index.php
@@ -9,12 +9,7 @@ if (!class_exists('Npcink_Page_Comment')) {
     {
         public static function run($option)
         {
-            //圆角彩色背景标签云
-            $color_tag = MaMi_Admin::get_config($option, 'color_tag');
-            if ($color_tag === true) {
-                require_once plugin_dir_path(__FILE__) . 'color_tags.php';
-                Npcink_Page_Color_Tags::run();
-            }
+           
 
             //评论区添加表情
             $comment_emote = MaMi_Admin::get_config($option, 'comment_emote');
--- admin/partials/page/function/index.php
+++ admin/partials/page/function/index.php
@@ -21,6 +21,13 @@ if (!class_exists('Npcink_Page_Function')) {
                 require_once plugin_dir_path(__FILE__) . 'single_remove_link.php';
                 Npcink_Single_Remove_Link::run();
             }
+
+            //圆角彩色背景标签云
+            $color_tag = MaMi_Admin::get_config($option, 'color_tag');
+            if ($color_tag === true) {
+                require_once plugin_dir_path(__FILE__) . 'color_tags.php';
+                Npcink_Page_Color_Tags::run();
+            }
         }
     }
 }
