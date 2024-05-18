3b9751ba23b0ae00609688725d566529b6f4fb22
Muze-mac <13581261474@163.com>
2024-05-18 11:29:14 +0800

    优化逻辑

--- admin/partials/function/seo/index.php
+++ admin/partials/function/seo/index.php
@@ -8,59 +8,25 @@
 if (!class_exists('Npcink_Easy_Seo')) {
     class Npcink_Easy_Seo
     {
-        private static $config;
+
         public static function run($option)
         {
-            self::$config = $option;
-            add_action('wp', array(__CLASS__, 'add_meta_home'));
+
+            //首页SEO
+            require_once plugin_dir_path(__FILE__) . 'seo_home.php'; //载入文件
+            Npcink_Seo_Home::run($option);
 
             //文章SEO
-            add_action('wp',  array(__CLASS__, 'single_seo'));
-        }
-
-        public static function add_meta_home()
-        {
-
-            //静态或动态首页
-            if (is_front_page()) {
-                //翻页是第一页
-                if (get_query_var('paged') < 2) {
-                    //准备选项
-                    $option = self::$config;
-                    //站点标题
-                    $title = MaBox_Admin::get_config($option, 'title');
-                    if ($title !== '' && $title !== false) {
-                        require_once plugin_dir_path(__FILE__) . 'site_title.php'; //载入文件
-                        Npcink_Seo_Site_Title::run($title);
-                    }
-
-                    //站点描述
-                    $description = MaBox_Admin::get_config($option, 'description');
-                    if ($description !== '' && $description !== false) {
-                        require_once plugin_dir_path(__FILE__) . 'site_description.php'; //载入文件
-                        Npcink_Seo_Site_Description::run($description);
-                    }
-
-                    //站点关键词
-                    $keywords = MaBox_Admin::get_config($option, 'keywords');
-                    if ($keywords !== '' && $keywords !== false) {
-                        require_once plugin_dir_path(__FILE__) . 'site_keywords.php'; //载入文件
-                        Npcink_Seo_Site_Keywords::run($keywords);
-                    }
-                }
-            }
-        }
-
-        //文章SEO
-        public static function single_seo()
-        {
-            $option = self::$config;
             $single = MaBox_Admin::get_config($option, 'single_seo');
             if ($single === true) {
                 require_once plugin_dir_path(__FILE__) . 'site_single.php'; //载入文件
                 Npcink_Seo_Single::run();
             }
         }
+
+
+
+
 
 
 
---
+++ admin/partials/function/seo/seo_home.php
@@ -0,0 +1,60 @@
+<?php
+//简单SEO - 首页TDK 
+if (!class_exists('Npcink_Seo_Home')) {
+    class Npcink_Seo_Home
+    {
+        private static $config;
+        public static function run($option)
+        {
+            self::$config = $option;
+            //添加首页标题需要先移除默认的，这里还做不到
+            add_action('wp', array(__CLASS__, 'add_meta_title'));
+            add_action('wp_head', array(__CLASS__, 'add_dk'), 1);
+        }
+
+        //添加首页关键词和描述
+        public static function add_dk()
+        {
+            //静态或动态首页
+            if (is_front_page()) {
+                //翻页是第一页
+                if (get_query_var('paged') < 2) {
+                    //准备选项
+                    $option = self::$config;
+                    //站点描述
+                    $description = MaBox_Admin::get_config($option, 'description');
+                    if ($description !== '' && $description !== false) {
+                        echo '<meta name="description" content="' . $description . '" />';
+                        echo "\n";
+                    }
+
+                    //站点关键词
+                    $keywords = MaBox_Admin::get_config($option, 'keywords');
+                    if ($keywords !== '' && $keywords !== false) {
+                        echo '<meta name="keywords" content="' . $keywords . '" />';
+                        echo "\n";
+                    }
+                }
+            }
+        }
+
+        //添加首页标题
+        public static function add_meta_title()
+        {
+            //静态或动态首页
+            if (is_front_page()) {
+                //翻页是第一页
+                if (get_query_var('paged') < 2) {
+                    //准备选项
+                    $option = self::$config;
+                    //站点标题
+                    $title = MaBox_Admin::get_config($option, 'title');
+                    if ($title !== '' && $title !== false) {
+                        require_once plugin_dir_path(__FILE__) . 'site_title.php'; //载入文件
+                        Npcink_Seo_Site_Title::run($title);
+                    }
+                }
+            }
+        }
+    }
+}
