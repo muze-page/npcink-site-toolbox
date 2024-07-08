08607130ab0dcf44aa29b8edc6ed5ffa24fea7f6
Muze-mac <13581261474@163.com>
2024-07-08 10:55:14 +0800

    添加短代码

--- admin/partials/shortcode/compose/index.php
+++ admin/partials/shortcode/compose/index.php
@@ -4,19 +4,21 @@
  * 功能：短代码 板式
  */
 if (!class_exists('MaBox_ShortCode_Compose')) {
-    class MaBox_ShortCode_Compose
+    class MaBox_ShortCode_Compose  extends MaBox_ShortCode
     {
-        public static function run($option)
+        public static function runs($option)
         {
-
             //文章列表
             $single_list = MaBox_Admin::get_config($option, 'single_list');
             if ($single_list === true) {
                 require_once plugin_dir_path(__FILE__) . 'single_list/index.php';
                 MaBox_ShortCode_Single_List::run();
+                //下拉中添加短代码
+                //这里需要进行转义，不然会丢失部分短代码内容
+                self::$option_list .= '
+                <option value="[past_posts_display ids=&quot;1,2,3&quot; limit=&quot;10&quot;]">文章列表</option>
+              ';
             }
         }
-
-       
     } //end
 }
--- admin/partials/shortcode/index.php
+++ admin/partials/shortcode/index.php
@@ -6,6 +6,9 @@
 if (!class_exists('MaBox_ShortCode')) {
     class MaBox_ShortCode
     {
+        //短代码下拉选项内容
+        public static $option_list = "";
+
         public static function run()
         {
 
@@ -17,10 +20,52 @@ if (!class_exists('MaBox_ShortCode')) {
              */
             require_once plugin_dir_path(__FILE__) . '/compose/index.php';
             $compose =  MaBox_Admin::get_config($config, 'compose');
-            MaBox_ShortCode_Compose::run($compose);
+            MaBox_ShortCode_Compose::runs($compose);
 
-            
+            //经典编辑器添加下拉按钮
+            if (!empty(self::$option_list)) {
+                add_action('admin_init', array(__CLASS__, 'custom_function_for_media_buttons'));
+            }
         }
-       
+
+        //经典编辑器，添加下拉按钮
+        public static function custom_function_for_media_buttons()
+        {
+            if (current_user_can('edit_posts') && current_user_can('edit_pages')) {
+                add_action('media_buttons', array(__CLASS__, 'wzt_select'), 11);
+                add_action('admin_head', array(__CLASS__, 'wzt_button'));
+            }
+        }
+        // 后台编辑器添加下拉式按钮
+        public static function wzt_select()
+        {
+            // 初始化选项列表，假设 self::$option_list 是一个静态变量并已经初始化
+            $options = self::$option_list;
+            // 开始构建下拉框的 HTML
+            $html = '
+  <select id="short_code_select">
+      <option value="">选择短代码</option>
+      ' . $options . '
+  </select>';
+
+            // 输出 HTML
+            echo $html;
+        }
+
+
+
+        public static function wzt_button()
+        {
+?>
+            <script type="text/javascript">
+                jQuery(document).ready(function($) {
+                    $("#short_code_select").change(function() {
+                        send_to_editor($("#short_code_select :selected").val());
+                        return false;
+                    });
+                });
+            </script>
+<?php
+        }
     } //end
 }
